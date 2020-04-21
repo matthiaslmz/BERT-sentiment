b7b5922707056ef985533389722dd4c43053b793
matthiaslmz <matthiaslmz@hotmail.com>
2020-04-20 23:10:45 -0400

    update README, created preprocessing script, and defined BERT model

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # BERT-sentiment
+
+The first part of this experiment is to use fine-tune BERT for sentiment classification on the [IMDB 50K reviews dataset](https://www.kaggle.com/lakshmi25npathi/imdb-dataset-of-50k-movie-reviews)
+
+# Dataset
+The motivation to using this dataset because it's already quite clean and has balanced classes: 50% positive and 50% Hence, minimal pre-processing is required.
+
---
+++ src/preprocess.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from transformers import BertTokenizer
+
+def preprocess_imdb(dataframe):
+    dataframe.review = dataframe.review.str.lower()
+    dataframe.review = dataframe.review.replace(regex=[r'<[^<]+?>', r'\[[^]]*\]'], value="")
+    dataframe.review = dataframe.review.replace(regex=[r'[^a-zA-Z]', r'\s+'], value=" ").strip()
+    dataframe.review = dataframe.review.str.strip().str.lower()
+    dataframe.sentiment = dataframe.sentiment.map({'positive': 1, 'negative': 0})
+
+    return dataframe
+
+
+class IMDBDataset:
+    def __init__(self, review, label):
+        self.review = review
+        self.label = label
+        self.tokenizer = BertTokenizer.from_pretrained("../models/bert-base-uncased", do_lower_case=True)
+
+    def __len__(self):
+        return len(self.review)
+
+    def _getitem__(self, item):
+        review = str(self.review[item])
+
+        encoded_inputs = tokenizer.encode_plus(review, add_special_tokens=True, max_length=512, pad_to_max_length=True, return_tensors='pt')
+
+    return None
