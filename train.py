e1aacb6ba30db09366fdf0c9a478ad1abc2a0f4c
matthiaslmz <matthiaslmz@hotmail.com>
2020-04-21 23:01:08 -0400

    add training script

---
+++ train.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import torch
+import logging
+from torch import nn
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn import model_selection
+import seaborn as sns
+import matplotlib.pyplot as plt
+from transformers import BertTokenizer
+from src.model import BERTSentiment
+from src.preprocess import preprocess_imdb, split_train_valid, IMDBDataset
+
+logging.basicConfig(level=logging.INFO)
+
+def main():
+
+    df = pd.read_csv("/misc/DLshare/home/rpzqk242/imdb_dataset.csv")
+    df = preprocess_imdb(df)
+    df_train, df_valid = split_train_valid(dataframe=df, test_size=0.1, random_state=420)
+
+    train_dataset = IMDBDataset(review=df_train.review.values, label=df_train.sentiment.values)
+    valid_dataset = IMDBDataset(review=df_valid.review.values, label=df_valid.sentiment.values)
+
+
+    model =  BERTSentiment(train_dset=train_dataset, eval_dset=valid_dataset)
+    model.train_model(ckpt_path="/misc/DLshare/home/rpzqk242/BERT-sentiment/checkpoints/checkpoint-%04d",
+                    output_path="/misc/DLshare/home/rpzqk242/BERT-sentiment/out/checkpoint-%04d")
+
+if __name__ == "__main__":
+    main()
