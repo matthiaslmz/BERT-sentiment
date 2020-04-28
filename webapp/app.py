1ba6cb9e2822f897c4677304a7f6f31b393ff4c1
matthiaslmz <matthiaslmz@hotmail.com>
2020-04-27 20:43:46 -0400

    add scripts for webapp

---
+++ webapp/app.py
@@ -0,0 +1,116 @@
+from flask import Flask, request, jsonify, render_template
+from wtforms import Form, TextAreaField, validators
+import numpy as np
+from transformers import BertForSequenceClassification, BertTokenizer
+import torch
+
+
+app = Flask(__name__)
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+saved_model_path = "C:/Users/MatthiasL/Desktop/DATA/ghdata/BERT-sentiment/checkpoints/checkpoint-11000/"
+model = BertForSequenceClassification.from_pretrained(saved_model_path)
+tokenizer = BertTokenizer.from_pretrained(saved_model_path)
+model.eval()
+
+class ReviewForm(Form):
+    sentence = TextAreaField('', [validators.DataRequired(), validators.length(min=15)])
+
+def sqlite_entry(path, document, y):
+    conn = sqlite3.connect(path)
+    c = conn.cursor()
+    c.execute("INSERT INTO review_db (review, sentiment, date)"\
+    " VALUES (?, ?, DATETIME('now'))", (document, y))
+    conn.commit()
+    conn.close()
+
+def encode_sentence(text, max_len=512):
+    review = str(text)
+    review = " ".join(review.split())
+    encoded_inputs = tokenizer.encode_plus(review, add_special_tokens=True, max_length=max_len, pad_to_max_length=True)
+    return encoded_inputs
+
+def text_pred(sentence, model, tokenizer):
+
+    DEVICE = "cuda"
+    model = model.to(DEVICE)
+
+    encoded_inputs = encode_sentence(sentence, max_len=512)
+    input_tensor = encoded_inputs['input_ids']
+    attention_tensor = encoded_inputs['attention_mask']
+
+    inputs = torch.tensor(input_tensor, dtype=torch.long).unsqueeze(0)
+    attens = torch.tensor(attention_tensor, dtype=torch.long).unsqueeze(0)
+
+    inputs = inputs.to(DEVICE)
+    attens = attens.to(DEVICE)
+
+    outputs = model(inputs, attention_mask=attens)
+    outputs = torch.sigmoid(outputs[0]).cpu().detach().numpy()
+
+    #results 
+    neg_pred = outputs[0][0]
+    pos_pred = 1- neg_pred
+    predicted_class = np.argmax(outputs)
+
+    return pos_pred, neg_pred, predicted_class
+
+@app.route('/')
+def index():
+    form = ReviewForm(request.form)
+    return render_template('reviewform.html', form=form)
+
+@app.route('/results', methods=['POST'])
+def results():
+    form = ReviewForm(request.form)
+    if request.method == 'POST' and form.validate():
+        review = request.form['sentence']
+        pos_pred, neg_pred, predicted_class = text_pred(review, model=model, tokenizer=tokenizer)
+    return render_template('results.html',
+                            content=review,
+                            pos_prob=round(pos_pred*100, 2),
+                            neg_prob= round(neg_pred*100, 2),
+                            prediction = predicted_class)
+    return render_template('reviewform.html', form=form)
+
+
+# @app.route("/predict")
+# def predict():
+#     txt = request.args.get("sentence")
+#     neg_pred = text_pred(txt, model=model, tokenizer=tokenizer)
+#     pos_pred = 1 - neg_pred
+#     return jsonify({'sentence': txt, 'positive': str(pos_pred), 'negative': str(neg_pred)})
+
+if __name__ == "__main__":
+    app.run(port=8007, debug=True)
+
+    #http://localhost:59829/predict?sentence=amazing!
+
+    # review = str("i love this video!")
+    # review = " ".join(review.split())
+    # review
+
+    # encoded_inputs = tokenizer.encode_plus(review, add_special_tokens=True, max_length=512, pad_to_max_length=True)
+    # encoded_inputs
+
+    # input_tensor = encoded_inputs['input_ids']
+    # attention_tensor = encoded_inputs['attention_mask']
+
+    # inputs = torch.tensor(input_tensor, dtype=torch.long).unsqueeze(0)
+    # masks = torch.tensor(attention_tensor, dtype=torch.long).unsqueeze(0)
+
+    # inputs = inputs.to(DEVICE)
+    # masks = masks.to(DEVICE)
+
+    # outputs = model(inputs, attention_mask=masks)
+    # outputs = torch.sigmoid(outputs[0]).cpu().detach().numpy()
+    # import numpy as np
+    # outputs[0]
+    # inv_label = {'negative': 0, 'positive': 1}
+    # inv_label[np.argmax(outputs)]
+
+    # np.argmax(outputs, 1).value
+
+    # outputs[0][0]
+
+
---
+++ webapp/database.py
@@ -0,0 +1,26 @@
+import sqlite3
+import os 
+
+conn = sqlite3.connect('movie_reviews.sqlite')
+
+c = conn.cursor()
+c.execute('DROP TABLE IF EXISTS review_db')
+c.execute('CREATE TABLE review_db' '(review TEXT, sentiment INTEGER, date TEXT)')
+
+example1 = 'The Phantom Menace is the best Star Wars movie ever!'
+c.execute("INSERT INTO review_db"\
+" (review, sentiment, date) VALUES"\
+" (?, ?, DATETIME('now'))", (example1, 1))
+
+example2 = 'I disliked the latest Star Trek movie'
+c.execute("INSERT INTO review_db"\
+" (review, sentiment, date) VALUES"\
+" (?, ?, DATETIME('now'))", (example2, 0))
+conn.commit()
+conn.close()
+
+c = conn.cursor()
+c.execute("SELECT * FROM review_db WHERE date"\
+" BETWEEN '2017-01-01 00:00:00' AND DATETIME('now')")
+results = c.fetchall()
+print(results)
